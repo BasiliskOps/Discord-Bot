4ea85e7b50346a21ef18f64c3e31eae091db8a2a
anathemacyber <noro.nero@gmail.com>
2021-02-20 01:12:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import discord
+import os
+import requests
+import json
+import random
+from replit import db 
+from keep_alive import keep_alive
+
+
+client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "meloncholy", "sadness", "lethargic", "morose", "beside myself", "grief", "death", "pain", "dying", "abyss", "afraid", "die", "suffering", "Die", "Death", "Suffering", "Suffer", "suffer"]
+
+starter_encouragements = ["Overcome thy suffering!", "Become the light and the way.", "Doth not the light of the Sun shine upon thee?", "Abandon doubt, for death is always smiling... fly true.", "Alas, though the Abyss may be Immaterium everlasting... the light of thy soul shines further, still.", "Let thy love be ever eternal...", "Oh what dreams may come!", "We'll meet again..."]
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+
+def delete_encouragement(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+    db["encouragements"] = encouragements 
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+      return
+
+  msg = message.content
+
+  if message.content.startswith('$motivate me darling'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  options = starter_encouragements
+  if "encouragements" in db.keys():
+    options = options + db["encouragements"]
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(options))
+
+  if msg.startswith("$new"):
+    encouraging_message = msg.split("$new ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added.")
+
+  if msg.startswith("$del"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("$del",1)[1])
+      delete_encouragement(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
